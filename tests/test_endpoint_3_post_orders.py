d53ff5d0257616cdc80bbcaedd47a986d1115a15
someuser <someuser@somehost.fake>
2021-03-28 13:16:31 +0300

    More tests for post::/couriers and post::/orders endpoints added (bad requests, 400 error code)

---
+++ tests/test_endpoint_3_post_orders.py
@@ -0,0 +1,268 @@
+import sys
+sys.path.append("../app")
+
+from fastapi.testclient import TestClient
+from main import app
+
+
+client = TestClient(app)
+
+
+def test_bad_request_empty_body():
+    response = client.post("/orders")
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_some_digits():
+    response = client.post("/orders", data="123")
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_some_chars():
+    response = client.post("/orders", data="data")
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_empty_json():
+    response = client.post("/orders",
+        json={},
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_json_with_some_random_stuff():
+    response = client.post("/orders",
+        json={"foo": "bar"},
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_json_with_bad_data_value():
+    response = client.post("/orders",
+        json={"data": False},
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_json_with_empty_data_array():
+    response = client.post("/orders",
+        json={"data": []},
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_json_with_bad_items_in_data_array():
+    response = client.post("/orders",
+        json={
+            "data": [
+                {
+                    "abc": 1
+                },
+                {
+                    "def": True
+                }
+            ]
+        },
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": []
+        }
+    }
+
+
+def test_bad_request_json_data_array_one_id_only():
+    response = client.post("/orders",
+        json={
+            "data": [
+                {
+                    "order_id": 1
+                }
+            ]
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": [
+                {
+                    "id": 1
+                }
+            ]
+        }
+    }
+
+
+def test_bad_request_json_data_array_wrong_id():
+    response = client.post("/orders",
+        json={
+            "data": [
+                {
+                    "order_id": 0
+                }
+            ]
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": [
+                {
+                    "id": 0
+                }
+            ]
+        }
+    }
+
+
+def test_bad_request_json_data_array_missing_fields():
+    response = client.post("/orders",
+        json={
+            "data": [
+                {
+                    "order_id": 1,
+                    "region": 12,
+                    "delivery_hours": ["09:00-18:00"]
+                },
+                {
+                    "order_id": 2,
+                    "weight": 15,
+                    "delivery_hours": ["09:00-18:00"]
+                },
+                {
+                    "order_id": 3,
+                    "weight": 0.01,
+                    "region": 22,
+                }
+            ]
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": [
+                {"id": 1},
+                {"id": 2},
+                {"id": 3}
+            ]
+        }
+    }
+
+
+def test_bad_request_json_data_array_extra_fields():
+    response = client.post("/orders",
+        json={
+            "data": [
+                {
+                    "order_id": 1,
+                    "weight": 0.23,
+                    "region": 12,
+                    "color": "green",
+                    "delivery_hours": ["09:00-18:00"]
+                },
+                {
+                    "order_id": 2,
+                    "weight": 15,
+                    "region": 12,
+                    "quantity": 125,
+                    "delivery_hours": ["09:00-18:00"]
+                },
+                {
+                    "order_id": 3,
+                    "weight": 0.01,
+                    "region": 22,
+                    "delivery_hours": ["09:00-18:00"]
+                }
+            ]
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": [
+                {"id": 1},
+                {"id": 2}
+            ]
+        }
+    }
+
+
+def test_bad_request_json_data_array_all_fields_bad_values():
+    response = client.post("/orders",
+        json={
+            "data": [
+                {
+                    "order_id": 1,
+                    "weight": 100.23,
+                    "region": 12,
+                    "delivery_hours": ["09:00-18:00"]
+                },
+                {
+                    "order_id": 2,
+                    "weight": 15,
+                    "region": "Moscow",
+                    "delivery_hours": ["09:00-18:00"]
+                },
+                {
+                    "order_id": 3,
+                    "weight": 0.01,
+                    "region": 22,
+                    "delivery_hours": ["19:77-28:00"]
+                }
+            ]
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        "validation_error": {
+            "orders": [
+                {"id": 1},
+                {"id": 2},
+                {"id": 3}
+            ]
+        }
+    }
